a5da2fe44b6e01630d65780a9667b225bdb79e38
lucianofmacedo2-ctrl <luciano.fmacedo2@gmail.com>
2026-08-12 16:18:27 -0300

    feat: update favicon and set locale to pt-BR
    Add soccer ball emoji favicon using data URIs and ensure consistent rendering by setting the favicon programmatically on mount. Update document language to pt-BR.

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,20 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// Ensure soccer ball favicon is set on app mount
+const setSoccerFavicon = () => {
+  const faviconSvg = `data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"><text y=".9em" font-size="90">⚽</text></svg>`;
+  let link: HTMLLinkElement | null = document.querySelector("link[rel*='icon']");
+  if (!link) {
+    link = document.createElement('link');
+    link.rel = 'shortcut icon';
+    document.getElementsByTagName('head')[0].appendChild(link);
+  }
+  link.href = faviconSvg;
+};
+
+setSoccerFavicon();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
